816694b15e3031e7ee1af29ea5aeff4b040430d0
ZhangChenyang <zhangchenyang89524@gmail.com>
2014-09-23 14:46:30 -0400

    fix bug of memory leak
    change malloc() free() to more stable new() and delete []

--- dp_tseg.cpp
+++ dp_tseg.cpp
@@ -2,6 +2,7 @@
 //#define _USE_MATH_DEFINES
 #include <math.h>
 #include <vector>
+#include <iostream>
 #define Inf 9999999999.0
 
 using namespace std;
@@ -22,10 +23,11 @@ inline int a2(int i, int j, int h)
     return j*h + i;
 }
 
-vector<int> dpseg(double & e, 
-           const vector<vector<double> > & D, int K, int off)
+vector<int> dpseg(double & e,
+                  const vector<vector<double> > & D, int K, int off)
 {
     //printf("size of current D: %d\n", D.size());
+    //printf("current K : %d\n",K);
     vector<int> S;
     S.clear();
     if(K>D.size())
@@ -36,7 +38,7 @@ vector<int> dpseg(double & e,
     if(K==1)
     {
         vector<double> m;
-
+        
         for(int i = 0; i<M; i++)
         {
             double mean=0.0;
@@ -76,9 +78,16 @@ vector<int> dpseg(double & e,
             last = D.begin()+j+1;
             vector<vector<double> > D1(first,last);
             S1 = dpseg(e1,D1,K-1,off);
+	    //cout<<K-1<<endl;
+
             V[check]=1;
             C[check].e = e1;
-            C[check].S = S1;
+	    //cout<<"<->"<<endl;
+	    //cout<<S1.size()<<endl;
+	    //cout<<C[check].S.size()<<endl;
+	    //cout<<"->"<<endl;
+            C[check].S = vector<int>(S1);
+	    //cout<<"<-"<<endl;
         }
         check = a(off+j+1,off+D.size()-1,0,T,T);
         if(V[check]==1) {
@@ -121,7 +130,10 @@ void mexFunction(int nlhs, mxArray*plhs[],int nrhs, const mxArray*prhs[]) {
     for(int i = 0; i<K*T*T; i++) {
         V[i]=0;
     }
-    C = (Cache *)malloc(sizeof(Cache)*K*T*T);
+    C = new Cache [K*T*T];
+    //C = (Cache *)malloc(sizeof(Cache)*K*T*T);
+    for(int i = 0; i<K*T*T;i++)
+	    C[i].S = vector<int>();
     vector<vector<double> > D;
     // fill D using A
     for(int i = 0; i<T; i++) {
@@ -132,15 +144,18 @@ void mexFunction(int nlhs, mxArray*plhs[],int nrhs, const mxArray*prhs[]) {
         D.push_back(sample);
     }
     double e;
+    //printf("here");
     vector<int> S = dpseg(e, D, K, 0);
+    //printf("after");
     
     plhs[0] = mxCreateDoubleMatrix(K+1, 1, mxREAL);
     plhs[1] = mxCreateDoubleMatrix(1, 1, mxREAL);
     double *ret = mxGetPr(plhs[0]);
     if(S.size()!=K-1) {
-        printf("%d\n",S.size());
+        printf("%d %d\n",S.size(),K-1);
         mexErrMsgTxt("not valid length");
     }
+
     for(int i = 0; i<S.size();i++) {
         ret[i+1] = (double)S[i];
     }
@@ -149,7 +164,10 @@ void mexFunction(int nlhs, mxArray*plhs[],int nrhs, const mxArray*prhs[]) {
     ret = mxGetPr(plhs[1]);
     ret[0] = e;
     //printf("%lf\n",e);
+    delete [] C;
     free(V);
-    free(C);
+	
+    //free(C);
+    D.clear();
     return;
 }
